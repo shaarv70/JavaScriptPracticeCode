e3df961c123ea0845bc56ab6e84f74b663ff34ab
arvind <arvindsharma50480@gmail.com>
2024-09-03 22:17:23 +0530

    changes having switch

--- ConditionalExpressions.js
+++ ConditionalExpressions.js
@@ -1,6 +1,6 @@
-let a = prompt("Hey whats your age" )//this will prompt notification
+//let a = prompt("Hey whats your age" )//this will prompt notification
 //which will take the input and convert that input into string
-a=Number.parseInt(a)//converting string into number
+/*a=Number.parseInt(a)//converting string into number
 if(a>0)
 {
   alert("this is valid age") //it is used to print on the browser
@@ -16,8 +16,49 @@ else
 console.log("done")
 //ternary operator 
 let decision= a<18?"Not Drive":"drive"
-console.log(decision)
+console.log(decision)*/
 //switch case also works in the same way as java
+
+function checkAge(age){
+
+  if(age>=18){
+    console.log("you are old enough");
+  }
+  else if(age >15){
+
+    console.log("Sorry, you are not older enough");
+  }
+
+  else if(age<10){
+
+    console.log("you are kid");
+  }
+
+  else {
+    console.log("Bad chhoice");
+  }
+}
+checkAge(13);
+checkAge(24); 
+
+//switch -- with switch we can use break
+
+function checkDay(day){
+
+   switch(day){
+
+    case 1:console.log("monday");
+    break;
+    case 2:console.log("tueday");
+    break;
+    default:console.log("wrong choice");
+    break;
+  }
+}
+
+checkDay(2);
+checkDay(3);
+
 
 
 
