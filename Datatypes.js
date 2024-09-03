7820dfa460765ad2a84f7ca5c76745136807193b
arvind <arvindsharma50480@gmail.com>
2024-09-03 21:33:01 +0530

    Datatypes changes

--- Datatypes.js
+++ Datatypes.js
@@ -6,7 +6,7 @@ var c=null;
 var d=undefined;
 {
   var b="this"; 
-   let k="chenu";
+  let k="chenu";
 }
 //console.log(k);//it will throw error
 console.log(b);//it will also print this
@@ -17,5 +17,44 @@ console.log(b);//it will also print this
 //const value cannot be reinitilaized and redclared
 //const has to initialized at the time of declaring
 const author="Harry";
-author="Arvind"; //thirow error
+//author="Arvind"; thirow error
+//var should not be used in JS or issue with var
 
+var flag= "hey";
+var t1=4;
+
+if(t1>3){
+
+  var flag ="check";
+}
+
+console.log(flag); //cannot show flag original value again
+//so let is mostly used
+
+let msg= "Hi";
+let time=4;
+
+if(time>3){
+
+  let msg="check";
+}
+console.log(msg);
+msg="55";
+console.log(msg);
+
+
+
+function rel(){
+
+  var top="hello";
+  console.log(top);
+}
+//console.log(top); throw error
+
+d= "my arvind";
+console.log(d);
+
+var g
+console.log(typeof(g))
+g=null;
+console.log(g);
--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const prompt=require('prompt-sync')()  
-prompt("enter any number")  
+// script.js
+
+// Prompt the user to enter a number
+var num = prompt("Enter any number:");
+
+// Display the result on the webpage or log it to the console
+if (num !== null) { // If the user didn't cancel the prompt
+    document.write("You entered: " + num);
+    console.log("You entered: " + num);
+} else {
+    document.write("No input was provided.");
+    console.log("No input was provided.");
+}
