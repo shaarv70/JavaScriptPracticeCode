f1e5bd5d0bcea1d6981675d87dd67c615ddb1e50
Arvind <arvindsharma50480@gmail.com>
2024-11-06 23:56:24 +0530

    Some addition

--- Loops.js
+++ Loops.js
@@ -1,34 +1,36 @@
-const prompt= require('prompt-sync')()
+//const prompt= require('prompt-sync')()
 //for loop
-/*let sum=0;
-let num=prompt("Enter any number")
-num=Number.parseInt(num)
-for(let i=0;i<num;i++)
-  {
-    sum=sum+i
-  }
-console.log(sum)
-//factorial for 5
-let product=1;
-for (let i=1;i<=5;i++)
-  {
-    product*=i
-  }
-console.log("Factorial of 5 is:"+product)
-*/
+// let sum=0;
+// let num=prompt("Enter any number")
+// num=Number.parseInt(num)
+// for(let i=0;i<num;i++)
+//   {
+//     sum=sum+i
+//   }
+
+// console.log(sum)
+// //factorial for 5
+// let product=1;
+// for (let i=1;i<=5;i++)
+//   {
+//     product*=i
+//   }
+//   document.write("The factorial of the given number is:"+sum);
+// console.log("Factorial of 5 is:"+product)
+
 //forin loop   //it will iterate through the whole object
-let obj=
+ let obj=
   { 
     harry:23,
     shivi:34,
     arvind:67
   }
- /*   for (let a in obj)
+   for (let a in obj)
       {
         console.log(a)//it will print key one by one
         console.log(obj[a])//it will print value one by one
       }
-   */
+  
 
  //forof loop   //it will iterate through a partiular value
     for (let a of "93")    
@@ -50,4 +52,9 @@ for(const c in arr){
   console.log(arr[c]);
 }
 
+for(const c in "93") // print index
+{
+  console.log(c);
+}
+ 
      
