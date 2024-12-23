8e4e6e37d0f880db220ceadb855ea72dd2868deb
Arvind <arvindsharma50480@gmail.com>
2024-12-24 02:18:41 +0530

    some code

--- Encapsulation.js
+++ Encapsulation.js
@@ -7,8 +7,19 @@ class Person {
     getAge() {
       return this.#age;
     }
-  }
+
+    #getName(){
+       console.log(this.name); 
+    }
+    
+    returnMethod(){
+      return this.#getName();
+
+    }
+}
   
   const person = new Person("Alice", 25);
   console.log(person.getAge()); // Output: 25
+  person.returnMethod();
+  
   
